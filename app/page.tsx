a537a1fda1a1cf3dc42add3c7fbcea6cf781cd0c
Nogyboy <guamanc9@gmail.com>
2023-07-07 12:42:13 -0500

    Home Basic Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> AI-Powered Propmts</span>
+      </h1>
+      <p className="desc text-center">
+        Hi, AI is a powered technology that can help you to write better.
+        With every new prompt, you can discover new ideas and share your
+        thoughts with the world.
+      </p>
+
+      {/* Feed */}
+    </section>
+
   )
 }
 
